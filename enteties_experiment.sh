669a9b329d0871deda4785084efc0820dce55e80
EStroiu <re4nell@gmail.com>
2024-04-18 20:27:22 +0200

    fixed both experiments, work with config.cfg now

--- enteties_experiment.sh
+++ enteties_experiment.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 
+# Load necessary modules
 module load prun
 
+# Load configuration from config.cfg
 source config.cfg
 
 mkdir -p server_logs
 mkdir -p client_logs
 
 server_ip=$(ssh $server_node "hostname -I | cut -d ' ' -f1")
-echo "Starting server on $server_node at $server_ip:7979..."
-ssh $server_node "$entities_folder/entities/entities.x86_64 -batchmode -nographics -logStats true -statsFile $entities_folder/server_log.csv -playType Server > ./benchmark/server_logs/server_output.log 2>&1 &"
 
-server_pid=$(ssh $server_node "pgrep -f entities.x86_64")
-
+echo "Starting server on $server_node at $server_ip:7777..."
+ssh $server_node "${entities_folder}entities/entities.x86_64 -batchmode -nographics -logStats true -statsFile ${entities_logs}/server_log.csv -playType Server > ./unity-net-benchmark/server_logs/server_output.log 2>&1 &"
+# ssh node001 "/var/scratch/esu530/entities/entities.x86_64 -batchmode -nographics -logStats true -statsFile /var/scratch/esu530/server_log.csv -playType Server > ./unity-net-benchmark/server_logs/server_output.log 2>&1 &" &
 sleep 5
 
-# echo "Starting system monitoring script on $server_node..."
-# ssh $server_node "export PATH=\"/home/esu530/miniconda3/bin:$PATH\" && conda activate && python3 ./benchmark/system_monitor.py $server_pid &" &
-
-server_pid=$(ssh $server_node "pgrep -f '$entities_folder/entities/entities.x86_64 -batchmode -nographics'")
+server_pid=$(ssh $server_node "pgrep entities.x86_64")
+# ssh node001 "pgrep /var/scratch/esu530/entities/entities.x86_64"
 echo "Starting system monitoring script on $server_node..."
-ssh $server_node "python3 benchmark/system_monitor.py ./benchmark/system_logs/entities/system_log.log $server_pid &" &
+ssh $server_node "python3 unity-net-benchmark/system_monitor.py ./unity-net-benchmark/system_logs/entities/system_log_${num_players}p_${benchmark_duration}s.log $server_pid &" &
 
 echo "Starting clients on $client_node..."
 for i in $(seq 1 $num_players)
 do
     echo "Starting client $i..."
-    ssh $client_node "$entities_folder/entities/entities.x86_64 -batchmode -nographics -serverUrl $server_ip -logStats true -statsFile $entities_logs/player_log_$i.csv -userID $i -playType Client > ./benchmark/client_logs/client${i}_output.log 2>&1 &" &
-    sleep 3
+    ssh $client_node "${entities_folder}entities/entities.x86_64 -batchmode -nographics -serverUrl $server_ip -logStats true -statsFile ${entities_logs}/player_log_$i.csv -userID $i -playType Client > ./unity-net-benchmark/client_logs/client${i}_output.log 2>&1 &" &
+    sleep 2
 done
 
 sleep 5
@@ -36,32 +35,28 @@ echo "Benchmarking for $benchmark_duration seconds..."
 sleep $benchmark_duration
 
 echo "Stopping system monitoring script on $server_node..."
-ssh $server_node "pkill -f 'python3 benchmark/system_monitor.py'"
+ssh $server_node "pkill -f 'python3 unity-net-benchmark/system_monitor.py'"
 
 echo "Stopping server..."
 ssh $server_node "kill $server_pid"
 sleep 2
-# Check if server process is still running and force terminate if necessary
-ssh $server_node "pgrep -f entities.x86_64" && ssh $server_node "pkill -9 -f entities.x86_64"
+ssh $server_node "kill -0 $server_pid" && ssh $server_node "kill -9 $server_pid"
 
 echo "Stopping clients..."
 ssh $client_node "pkill -f entities.x86_64"
 sleep 2
-ssh $client_node "pgrep -f entities.x86_64" && ssh $client_node "pkill -9 -f entities.x86_64"
+ssh $client_node "pkill -0 -f entities.x86_64" && ssh $client_node "pkill -9 -f entities.x86_64"
 
 echo "Running collection script.."
 python3 ./entities_collect_script.py $entities_logs
 wait 10
 
-# Delete server and client logs
 echo "Deleting server and client logs..."
 rm -rf ./server_logs/*
 rm -rf ./client_logs/*
 
-Delete mirror logs folder
-echo "Deleting mirror logs folder..."
-rm -rf $entities_logs 
+echo "Deleting ECS logs..."
+rm ${entities_logs}/*
 
 echo "Benchmarking completed."
-
 echo "Script execution complete."
--- mirror_experiment.sh
+++ mirror_experiment.sh
@@ -12,19 +12,19 @@ mkdir -p client_logs
 server_ip=$(ssh $server_node "hostname -I | cut -d ' ' -f1")
 
 echo "Starting server on $server_node at $server_ip:7777..."
-ssh $server_node "$mirror_kcp_folder/mirror_kcp/mirror_kcp.x86_64 -batchmode -nographics -server > ./benchmark/server_logs/server_output.log 2>&1 &" &
+ssh $server_node "$mirror_kcp_folder/mirror_kcp/mirror_kcp.x86_64 -batchmode -nographics -server > ./unity-net-benchmark/server_logs/server_output.log 2>&1 &" &
 
 sleep 5
 
 server_pid=$(ssh $server_node "pgrep -f '$mirror_kcp_folder/mirror_kcp/mirror_kcp.x86_64 -batchmode -nographics -server'")
 echo "Starting system monitoring script on $server_node..."
-ssh $server_node "python3 benchmark/system_monitor.py ./benchmark/system_logs/mirror_kcp/system_log.log $server_pid &" &
+ssh $server_node "python3 unity-net-benchmark/system_monitor.py ./unity-net-benchmark/system_logs/mirror_kcp/system_log_${num_players}p_${benchmark_duration}s.log $server_pid &" &
 
 echo "Starting clients on $client_node..."
 for i in $(seq 1 $num_players)
 do
     echo "Starting client $i..."
-    ssh $client_node "$mirror_kcp_folder/mirror_kcp/mirror_kcp.x86_64 -batchmode -nographics -server_ip $server_ip -server_port 7777 -client > ./benchmark/client_logs/client${i}_output.log 2>&1 &" &
+    ssh $client_node "$mirror_kcp_folder/mirror_kcp/mirror_kcp.x86_64 -batchmode -nographics -server_ip $server_ip -server_port 7777 -client > ./unity-net-benchmark/client_logs/client${i}_output.log 2>&1 &" &
     sleep 2
 done
 
@@ -34,7 +34,7 @@ echo "Benchmarking for $benchmark_duration seconds..."
 sleep $benchmark_duration
 
 echo "Stopping system monitoring script on $server_node..."
-ssh $server_node "pkill -f 'python3 benchmark/system_monitor.py'"
+ssh $server_node "pkill -f 'python3 unity-net-benchmark/system_monitor.py'"
 
 echo "Stopping server..."
 ssh $server_node "kill $server_pid"
@@ -54,8 +54,8 @@ echo "Deleting server and client logs..."
 rm -rf ./server_logs/*
 rm -rf ./client_logs/*
 
-echo "Deleting mirror logs folder..."
-rm -rf $mirror_kcp_logs
+echo "Deleting mirror logs..."
+rm ${mirror_kcp_logs}/*
 
 echo "Benchmarking completed."
 echo "Script execution complete."
