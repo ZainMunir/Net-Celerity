e34f152302ffe981f107b1533728880c0244d421
EStroiu <re4nell@gmail.com>
2024-05-31 13:50:02 +0200

    A lot of changes to the plots

--- plot_results.py
+++ plot_results.py
@@ -2,8 +2,11 @@ import os
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import seaborn as sns
+from math import ceil
 
 plt.style.use('seaborn-v0_8-colorblind')
+sns.set_palette('colorblind')
 
 # Printing box plots
 def create_boxplots_rtt(data_dir):
@@ -23,7 +26,7 @@ def create_boxplots_rtt(data_dir):
 
             player_data[total_players].append(group['RoundTripDelay_ms'].values)
     
-    colors = ['orange', 'green', 'red', 'blue', 'purple', 'brown', 'pink', 'gray', 'olive', 'cyan']
+    colors = ['red', 'green', 'orange', 'blue', 'purple', 'brown', 'pink', 'gray', 'olive', 'cyan']
     for total_players, data in player_data.items():
         plt.figure(figsize=(6, 2))
         bp = plt.boxplot(data, vert=False, patch_artist=True, widths=0.7)  # Horizontal boxplots with different colors
@@ -36,13 +39,12 @@ def create_boxplots_rtt(data_dir):
         for outlier in bp['fliers']:
             outlier.set(marker='x', color='black', markersize=5, alpha=0.5)
 
-        plt.xlabel('round trip delay (ms)',fontsize=14)
+        plt.xlabel('round trip delay [ms]',fontsize=14)
         plt.title(f'{total_players} total players', fontsize=14)
         plt.yticks(range(1, len(csv_files) + 1), [os.path.splitext(file)[0].replace('_results', '') for file in csv_files], fontsize=14)
         plt.xticks(fontsize=14)
         plt.grid(axis='x')
         plt.tight_layout()
-        plt.show()
         plt.savefig(f'plots/rtt_comparison_{total_players}.pdf')
 
 def total_sent(system_logs_folder):
@@ -168,10 +170,10 @@ def cpu_usage_per_second(system_logs_folder):
                 player_numbers.add(player_number)
 
     for player_number in sorted(player_numbers):
-        plt.figure(figsize=(7, 4))
+        plt.figure(figsize=(5, 3))
         plt.title(f'{player_number} total players', fontsize=14)
-        plt.xlabel("time (s)", fontsize=14)
-        plt.ylabel("CPU usage (%)", fontsize=14)
+        plt.xlabel("time [s]", fontsize=14)
+        plt.ylabel("CPU usage [%]", fontsize=14)
         colors = ['red', 'green', 'orange', 'red', 'purple', 'brown', 'pink', 'gray', 'olive', 'cyan']
         color_index = 0
 
@@ -192,7 +194,8 @@ def cpu_usage_per_second(system_logs_folder):
                         plt.plot(df.index, df['proc.cpu_percent'], label=f"{prototype_folder}", linewidth=2, color=colors[color_index], alpha=0.8)
                         color_index += 1
 
-        plt.legend(bbox_to_anchor=(0,1.06,1,0.2), loc="lower left", mode="expand", ncol=3,fontsize=14,frameon=False)
+        # plt.legend(bbox_to_anchor=(0,1.06,1,0.2), loc="lower left", mode="expand", ncol=3,fontsize=14,frameon=False)
+        plt.title(f'{player_number} total players', fontsize=14)
         plt.grid(axis='y')
         plt.tight_layout()
         plt.xticks(fontsize=14)
@@ -272,9 +275,129 @@ def rss_ram_usage_plots(system_logs_folder):
     plt.tight_layout()
     plt.savefig(f'plots/rss_ram_usage.pdf')
 
-# rss_ram_usage_plots("system_logs")
 
-# cpu_usage_per_second("system_logs")
-create_boxplots_rtt('.')
+def create_combined_boxplot_rtt(data_dir):
+    # Collect all CSV files
+    csv_files = [file for file in os.listdir(data_dir) if file.endswith('_results.csv')]
+    all_data = []
+
+    # Read data from each CSV and append to the list
+    for csv_file in csv_files:
+        experiment_name = os.path.splitext(csv_file)[0].replace('_results', '')
+        df = pd.read_csv(os.path.join(data_dir, csv_file))
+        df['Experiment'] = experiment_name
+        all_data.append(df)
+    
+    # Concatenate all data into a single DataFrame
+    combined_df = pd.concat(all_data)
+
+    # Set up the plot
+    plt.figure(figsize=(6, 4))
+    sns.boxplot(
+        x='Total_Players', 
+        y='RoundTripDelay_ms', 
+        hue='Experiment', 
+        data=combined_df,
+        palette=['red', 'green', 'orange'],
+        showfliers=False
+    )
+
+    plt.xlabel('number of players', fontsize=14)
+    plt.ylabel('round trip delay [ms]', fontsize=14)
+    plt.xticks(ticks=[0, 1, 2, 3, 4, 5], labels=[5, 10, 20, 40, 60, 80], fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.legend(fontsize=14)
+    plt.grid(axis='y')
+
+    plt.tight_layout()
+    plt.savefig('plots/rtt_combined_comparison.pdf')
+
+def calculate_outliers(data):
+    # Ensure the data is a pandas Series and numeric
+    data = pd.Series(data)
+    data = pd.to_numeric(data, errors='coerce').dropna()
+
+    if len(data) == 0:
+        return pd.Series([])  # Return an empty series if data is empty after conversion
+
+    q1 = np.percentile(data, 25)
+    q3 = np.percentile(data, 75)
+    iqr = q3 - q1
+    lower_bound = q1 - 1.5 * iqr
+    upper_bound = q3 + 1.5 * iqr
+    return data[(data < lower_bound) | (data > upper_bound)]
+
+def calculate_outliers(data):
+    # Ensure the data is a pandas Series and numeric
+    data = data.apply(lambda x: ceil(x))
+
+    data = pd.Series(data)
+    data = pd.to_numeric(data, errors='coerce').dropna()
+
+    if len(data) == 0:
+        return pd.Series([])  # Return an empty series if data is empty after conversion
+
+    q1 = np.percentile(data, 25)
+    q3 = np.percentile(data, 75)
+    iqr = q3 - q1
+    lower_bound = q1 - 1.5 * iqr
+    upper_bound = q3 + 1.5 * iqr
+    return data[(data < lower_bound) | (data > upper_bound)]
+
+def create_outliers_cdf_plot(data_dir):
+    csv_files = [file for file in os.listdir(data_dir) if file.endswith('_results.csv')]
+    all_outliers = []
+
+    for csv_file in csv_files:
+        experiment_name = os.path.splitext(csv_file)[0].replace('_results', '')
+        file_path = os.path.join(data_dir, csv_file)
+    
+        df = pd.read_csv(file_path)
+        
+        grouped = df.groupby('Total_Players')
+        for total_players, group in grouped:
+            outliers = calculate_outliers(group['RoundTripDelay_ms'])
+            if not outliers.empty:
+                outliers_df = pd.DataFrame({'RoundTripDelay_ms': outliers, 'Prototype': experiment_name, 'Total_Players': total_players})
+                all_outliers.append(outliers_df)
+    
+    if not all_outliers:
+        print("No outliers found in any of the datasets.")
+        return
+
+    combined_outliers_df = pd.concat(all_outliers)
+
+    
+
+    sns.set_palette('colorblind')
+
+    line_styles = ['-', '--', '-.', ':']
+    line_width = 2.5
+
+    # Set up the plot for each prototype
+    for prototype in combined_outliers_df['Prototype'].unique():
+        plt.figure(figsize=(7, 3))
+        
+        prototype_outliers = combined_outliers_df[combined_outliers_df['Prototype'] == prototype]
+        for i, total_players in enumerate(prototype_outliers['Total_Players'].unique()):
+            player_outliers = prototype_outliers[prototype_outliers['Total_Players'] == total_players]['RoundTripDelay_ms']
+            sns.ecdfplot(player_outliers, label=f'{total_players} players', linestyle=line_styles[i % len(line_styles)], linewidth=line_width)
+        
+        plt.xlabel('round trip delay [ms]', fontsize=14)
+        plt.ylabel('probability density', fontsize=14)
+        plt.xticks(fontsize=14)
+        plt.yticks(fontsize=14)
+        plt.legend(fontsize=14)
+        plt.grid(True)
+
+        plt.tight_layout()
+        plt.savefig(f'plots/rtt_outliers_cdf_comparison_{prototype}.pdf')
+
+
+# rss_ram_usage_plots("system_logs")
+# cpu_usage_per_second("./system_logs_workload1")
+# create_boxplots_rtt('./workload2_results')
 # total_sent("system_logs")
 # total_recv("system_logs")
+create_outliers_cdf_plot('./workload2_results')
+create_combined_boxplot_rtt('./workload2_results')
--- time-analysis.py
+++ time-analysis.py
@@ -23,66 +23,35 @@ def extract_var_csv(folder_path):
 
     return csv_files
 
-print(extract_var_csv('./'))
-
 data = [('mirrorTelepathy', 420, 36.48168895725908),('mirrorTelepathy', 390, 38.432580244694925), ('mirrorTelepathy', 360, 19.61400659312184), ('mirrorTelepathy', 330, 27.385573279756102), ('mirrorTelepathy', 300, 31.199244309183964), ('mirrorTelepathy', 270, 8.985656426445209), ('mirrorTelepathy', 240, 17.447610396295364), ('mirrorTelepathy', 210, 30.183195278196145), ('mirrorTelepathy', 180, 15.873806006125658), ('mirrorTelepathy', 150, 11.419250185575002), ('mirrorTelepathy', 120, 20.2018314046651), ('mirrorTelepathy', 90, 26.937288423081238), ('mirrorTelepathy', 60, 47.66263981263731),('mirrorTelepathy', 30, 26.347890817833605), ('mirrorTelepathy', 5, 21.38757258774539), ('mirrorKCP', 420, 28.16841010949316), ('entities', 420, 2.0514378952505137), ('mirrorKCP', 390, 27.88917162982882), ('entities', 390, 2.767924758625096), ('entities', 360, 2.3936186621560704), ('mirrorKCP', 360, 23.529665750464645), ('entities', 330, 2.946337365386775), ('mirrorKCP', 330, 26.01320123210872), ('entities', 300, 2.1961736552229403), ('mirrorKCP', 300, 23.498996340916346), ('entities', 270, 2.0857472677203184), ('mirrorKCP', 270, 25.47688914525538), ('entities', 240, 2.248842759005533), ('mirrorKCP', 240, 27.155120483658706), ('entities', 210, 2.2380403169105634), ('mirrorKCP', 210, 26.015050820684355), ('mirrorKCP', 150, 23.059733797866652), ('mirrorKCP', 30, 19.41311574689418), ('mirrorKCP', 5, 25.773749956061987), ('mirrorKCP', 60, 20.85262234652452), ('mirrorKCP', 120, 19.54594182451042), ('entities', 60, 1.854112620557412), ('mirrorKCP', 180, 24.42828712842906), ('entities', 150, 2.2530320338136622), ('entities', 5, 6.144624665252794), ('mirrorKCP', 90, 26.496122952786468), ('entities', 120, 2.582409636103802), ('entities', 180, 3.121041116714914), ('entities', 30, 4.3650943895019), ('entities', 90, 3.292065501278577)]
 sorted_data = sorted(data, key=lambda x: x[1])
 
 x_ticks = []
 for x in sorted_data:
-    if x[1] not in x_ticks:
-        x_ticks.append(x[1])
+    if x[1] % 60 == 0:  # Check if the value is a multiple of 60 (representing minutes)
+        x_ticks.append(x[1] // 60)  # Append the value in minutes
 
 category_data = defaultdict(list)
 
 for entry in sorted_data:
     category_data[entry[0]].append((entry[1], entry[2]))
 
-# plt.figure(figsize=(14, 3))
-
-# markers = ['D', 's', 'o', '^', 'v', 'p', 'P', '*', 'X', 'd', 'H', 'h', '<', '>', '8']
-# colors = ['orange', 'g', 'r', 'c', 'm', 'y', 'k']
-# current_index = 0
-# for category, data_points in category_data.items():
-#     x_values = [x for x, _ in data_points]
-#     y_values = [y for _, y in data_points]    
-#     plt.plot(x_values, y_values, label=category, marker=markers[current_index], color=colors[current_index])
-#     current_index += 1
-
-# # set y-axis grid
-# plt.grid(axis='y')
-# plt.xlabel('time (s)',fontsize=14)
-# plt.ylabel('variance of RTT',fontsize=14)
-# # plt.legend(bbox_to_anchor=(0,1,0.6,0), loc="lower left", mode="expand", ncol=3,fontsize=14,frameon=False)
-
-# # plt.xticks([0, 5, 30, 60, 90, 120, 150, 180, 240])
-# plt.xticks(x_ticks, fontsize=14)
-# plt.yticks(fontsize=14)
-
-# if not os.path.exists('plots'):
-#     os.makedirs('plots')
-
-# plt.savefig('plots/variance_plot.pdf')
-# Create a subplot with specified height
 fig, ax = plt.subplots(figsize=(14, 4))
 
-# Define markers and colors
 markers = ['D', 's', 'o', '^', 'v', 'p', 'P', '*', 'X', 'd', 'H', 'h', '<', '>', '8']
 colors = ['orange', 'g', 'r', 'c', 'm', 'y', 'k']
 current_index = 0
 
-# Plot data for each category
 for category, data_points in category_data.items():
-    x_values = [x for x, _ in data_points]
+    x_values = [x/60 for x, _ in data_points]  # Converting seconds to minutes
     y_values = [y for _, y in data_points]    
     ax.plot(x_values, y_values, label=category, marker=markers[current_index], color=colors[current_index])
     current_index += 1
 
-# Set y-axis grid
-ax.grid(axis='y')
-ax.set_xlabel('time (s)', fontsize=14)
-ax.set_ylabel('variance of RTT', fontsize=14)
-ax.set_xticks(x_ticks)
+ax.grid(axis='both')
+ax.set_xlabel('time [minutes]', fontsize=14)  # Updating xlabel
+ax.set_ylabel('variance of RTT [ms$^2$]', fontsize=14)  # Updating ylabel with LaTeX
+ax.set_xticks(x_ticks)  # Using only integer values for x-axis
 ax.tick_params(axis='x', labelsize=14)
 ax.tick_params(axis='y', labelsize=14)
 plt.legend(bbox_to_anchor=(0,1.02,0.6,0.2), loc="lower left", mode="expand", ncol=3,fontsize=14,frameon=False)
@@ -92,4 +61,3 @@ plt.tight_layout()
 if not os.path.exists('plots'):
     os.makedirs('plots')
 plt.savefig('plots/variance_plot.pdf')
-plt.show()
